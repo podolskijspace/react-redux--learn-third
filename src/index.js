eb7fe9341d2f571f29410a905212e01b48eb1f58
podolskijspace <goncharovnikita713@gmail.com>
2021-09-08 16:39:50 +0300

    Создал основу будущего приложения

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducer';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry';
+import RestoService from './services/resto-service';
+import RestoServiceContext from './components/resto-service-context';
+import store from './store';
 
-const store = createStore(reducer); 
+import './index.scss';
+
+const restoService = new RestoService();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+    <ErrorBoundry >
+      <RestoServiceContext.Provider value={restoService}>
+        <Router >
+          <App />
+        </Router>
+      </RestoServiceContext.Provider>
+    </ErrorBoundry>
+  </Provider>
+  , document.getElementById('root'));
+
