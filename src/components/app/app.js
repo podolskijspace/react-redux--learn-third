97ff8ff55c37dcb710b0cb45afb4d73754465183
podolskijspace <goncharovnikita713@gmail.com>
2021-09-08 17:24:39 +0300

    Получили данные из базы данных

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc';
 
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
+const App = () => {
   return (
     <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-      <AppHeader total={50}/>
-      <MainPage/>
-      <CartPage/>
+        <AppHeader total={50}/>
+        <MainPage/>
+        <CartPage/>
     </div>
   )
 }
 
-export default WithRestoService()(App);
+export default App;
