0bc6176db58d1c182799f5349a6931107933dcfe
podolskijspace <goncharovnikita713@gmail.com>
2021-09-08 18:54:04 +0300

    Добавил удаление в корзину и удаление из корзины

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,23 @@ const menuRequested = () => {
   }
 }
 
+const addedToCart =(id) => {
+  return {
+    type: 'ITEM_ADD_TO_CART',
+    payload: id
+  }
+}
+
+const deleteFromCart =(id) => {
+  return {
+    type: 'ITEM_REMOVE_FROM_CART',
+    payload: id
+  }
+}
+
 export {
   menuLoaded,
   menuRequested,
+  addedToCart,
+  deleteFromCart
 }
