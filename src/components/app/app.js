770e6578762944de98b3c7debca2d815fbdb4da4
podolskijspace <goncharovnikita713@gmail.com>
2021-09-08 18:15:18 +0300

    Добавил навигацию по страницам

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,13 +3,14 @@ import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 
 import Background from './food-bg.jpg';
+import { Route } from 'react-router';
 
 const App = () => {
   return (
     <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
         <AppHeader total={50}/>
-        <MainPage/>
-        <CartPage/>
+        <Route path="/" exact component={MainPage}/>
+        <Route path="/cart/" exact component={CartPage}/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 import ErrorBoundry from './components/error-boundry';
 import RestoService from './services/resto-service';
 import RestoServiceContext from './components/resto-service-context';
@@ -13,14 +13,16 @@ import './index.scss';
 const restoService = new RestoService();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ErrorBoundry >
-      <RestoServiceContext.Provider value={restoService}>
-        <Router >
-          <App />
-        </Router>
-      </RestoServiceContext.Provider>
-    </ErrorBoundry>
-  </Provider>
+  <BrowserRouter >
+    <Provider store={store}>
+      <ErrorBoundry >
+        <RestoServiceContext.Provider value={restoService}>
+          <Router >
+            <App />
+          </Router>
+        </RestoServiceContext.Provider>
+      </ErrorBoundry>
+    </Provider>
+  </BrowserRouter>
   , document.getElementById('root'));
 
