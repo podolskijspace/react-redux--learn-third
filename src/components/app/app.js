3c1c80df0fbf7917f5d69889f2df419b9046c229
podolskijspace <goncharovnikita713@gmail.com>
2021-09-09 12:57:13 +0300

    Добавил измениен цены заказа

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import { connect } from 'react-redux';
 
 import Background from './food-bg.jpg';
 import { Route } from 'react-router';
 
-const App = () => {
+const App = ({orderPrice}) => {
   return (
     <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-        <AppHeader total={50}/>
+        <AppHeader total={orderPrice}/>
         <Route path="/" exact component={MainPage}/>
         <Route path="/cart/" exact component={CartPage}/>
     </div>
   )
 }
 
-export default App;
+const mapStateToProps = ({orderPrice}) => {
+  return {
+    orderPrice,
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ const initialState = {
   menu: [],
   loading: true,
   items: [],
-  
+  orderPrice: 0,
 }
 
 const reducer = (state = initialState, action) => {
@@ -20,10 +20,11 @@ const reducer = (state = initialState, action) => {
         loading: true,
       }
     case 'ITEM_ADD_TO_CART':
-      const id = action.payload;
-      const item = state.menu.find(item => item.id === id);
-
-      let newItem = {
+      const id = action.payload,
+            item = state.menu.find(item => item.id === id);
+            
+      let newObjA = null,
+          newItem = {
         title: item.title,
         price: item.price,
         url: item.url,
@@ -32,13 +33,10 @@ const reducer = (state = initialState, action) => {
       };
 
       const semItemIndex = state.items.findIndex(item => item.id === id);
-
-      console.log(~semItemIndex);
       if (~semItemIndex) {
         newItem.count += state.items[semItemIndex].count;
-        console.log(newItem);
 
-        return {
+        newObjA = {
           ...state,
           items: [
             ...state.items.slice(0, semItemIndex),
@@ -47,23 +45,38 @@ const reducer = (state = initialState, action) => {
           ]
         }
       }
-
-      return {
-        ...state,
-        items: [
-          ...state.items, newItem
-        ]
+      else {
+        newObjA = {
+          ...state,
+          items: [
+            ...state.items, newItem
+          ]
+        }
       }
+
+      newObjA.orderPrice = newObjA.items.reduce((sum, current) => {
+        return current.count * current.price + sum;
+      }, 0);
+      
+      return newObjA;
+
     case 'ITEM_REMOVE_FROM_CART':
       const idx = action.payload;
       const itemIndex = state.items.findIndex(item=> item.id === idx)
-      return {
+      const newObjD = {
         ...state,
         items: [
           ...state.items.slice(0, itemIndex),
           ...state.items.slice(itemIndex + 1)
         ]
       }
+      
+      newObjD.orderPrice = newObjD.items.reduce((sum, current) => {
+        return current.count * current.price + sum;
+      }, 0);
+    
+    return newObjD;
+
     default:
       return state;
   }
