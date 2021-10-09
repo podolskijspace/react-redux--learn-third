021b800dc659a3e52dd51482eaa7e6057de11e5b
podolskijspace <goncharovnikita713@gmail.com>
2021-10-09 09:26:03 +0300

    Исправил проблему с созданием элементов каждый раз и масштабирование в IOS

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,30 @@ import React, {Component} from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import { connect } from 'react-redux';
-
+import WithRestoService from '../hoc';
+import {menuLoaded, menuRequested} from '../../actions';
 import { Route } from 'react-router';
+import Spinner from "../spinner";
+import './app.scss';
 
 class App extends Component {
+  componentDidMount () {
+    this.props.menuRequested();
+    const {RestoService} = this.props;
+
+    // RestoService.getGoods({id: ["0c4aab30", "1efa7e46"]})
+    //   .then(res => this.props.menuLoaded(res));
+
+    RestoService.getGoods()
+      .then(res => this.props.menuLoaded(res));
+  }
+
   render () {
-    const {orderPrice} = this.props;
+    const {orderPrice, loading} = this.props;
+
+    if (loading) {
+      return <Spinner />
+    }
 
     return (
       <div style={{background: `rgba(0,0,0, .4)`}} className="app">
@@ -19,11 +37,17 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({orderPrice}) => {
+const mapStateToProps = (state) => {
   return {
-    orderPrice,
+    menuItems: state.menu,
+    loading: state.loading,
+    orderPrice: state.orderPrice
   }
 }
 
+const mapDispatchToProps = {
+    menuLoaded, menuRequested
+  }
 
-export default connect(mapStateToProps)(App);
+
+export default WithRestoService () (connect(mapStateToProps, mapDispatchToProps)(App));
--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,36 +1,18 @@
 import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import { connect } from 'react-redux';
-import WithRestoService from '../hoc';
-import {menuLoaded, menuRequested, addedToCart} from '../../actions';
-import Spinner from '../spinner'
+import {addedToCart} from '../../actions';
 
 import './menu-list.scss';
 
 class MenuList extends Component {
-  componentDidMount () {
-    this.props.menuRequested();
-    const {RestoService} = this.props;
-
-    // RestoService.getGoods({id: ["0c4aab30", "1efa7e46"]})
-    //   .then(res => this.props.menuLoaded(res));
-
-    RestoService.getGoods()
-      .then(res => this.props.menuLoaded(res));
-  }
-
   render() {
-    const {menuItems, loading, addedToCart} = this.props;
-
-    if (loading) {
-      return <Spinner />
-    }
+    const {menuItems, addedToCart} = this.props;
 
     return (
       <ul className="menu__list">
         {
           menuItems.map(menuItem => {
-            console.log(menuItem)
             return <MenuListItem key={menuItem.id} menuItem={menuItem} onAddToCart={() => addedToCart(menuItem.id)}/>;
           })
         }
@@ -42,14 +24,11 @@ class MenuList extends Component {
 const mapStateToProps = (state) => {
   return {
     menuItems: state.menu,
-    loading: state.loading,
   }
 }
 
 const mapDispatchToProps = {
-  menuLoaded,
-  menuRequested,
   addedToCart,
 }
 
-export default WithRestoService () (connect(mapStateToProps, mapDispatchToProps)(MenuList));
+export default connect(mapStateToProps, mapDispatchToProps)(MenuList);
